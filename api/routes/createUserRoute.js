6d1255caaa00e9007a4b63bacad42120ce4f388a
andressalazar08 <andres.f.salazar@correounivalle.edu.co>
2024-09-21 15:08:48 -0500

    new user validation middleware

--- api/routes/createUserRoute.js
+++ api/routes/createUserRoute.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const { registerUser } = require('../controllers/userController');
 const router = express.Router();
+const { newUserValidation } = require('../middlewares/userValidate');
 
-router.post('/registerUser', registerUser);
+
+router.post('/registerUser', newUserValidation,registerUser);
 
 module.exports = router;
