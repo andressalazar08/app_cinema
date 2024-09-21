db268885b73634bcc651a21ee260125f1dbf5218
andressalazar08 <andres.f.salazar@correounivalle.edu.co>
2024-09-21 14:57:55 -0500

    catch Async Function middleware

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,11 +1,12 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const { catchAsyncErrors } = require('../middlewares/cathcAsync');
 
-const registerUser = async(req, res)=>{
+const registerUser = catchAsyncErrors(async(req, res)=>{
 
     const { name, email, password, profile } = req.body;
 
-    try{
+
         const existingUSer = await User.findOne({where:{email:email}});
         if(existingUSer){
             return res.status(400).json({message:'User already exists, cannot create a user with this email'});
@@ -28,13 +29,7 @@ const registerUser = async(req, res)=>{
         })
         return res.status(201).json({message:'User registered successfully', user:newUser});
 
-    }catch(error){
-        console.log('Error on register user', error);
-        return res.status(500).json({message:'Internal server error', error:error.message})
-    }
-
-
-};
+});
 
 module.exports = { 
     registerUser,
---
+++ api/middlewares/cathcAsync.js
@@ -0,0 +1,8 @@
+const catchAsyncErrors = (controller)=>{
+    return function(req, res, next){
+        controller(req, res).catch((err)=>{
+            next(err);//express default error handler
+        })
+    }
+}
+module.exports ={catchAsyncErrors};
