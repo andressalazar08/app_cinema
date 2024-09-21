810592cd83e75e8f13d70052e03d1d567702796c
andressalazar08 <andres.f.salazar@correounivalle.edu.co>
2024-09-21 14:45:47 -0500

    app restructure

---
+++ api/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv').config();
+const { Sequelize }= require('sequelize');
+const sequelize = require('./config/database');
+
+
+const app = express();
+
+app.use(express.json());//req.body usefull
+
+app.use(cors());
+
+const router = express.Router();
+
+router.get('/', (req,res)=>{
+    res.json({
+        message: 'API estructuras de datos - cinema'
+    })
+});
+
+app.use('/', router);
+
+//Ruta movies
+const movieRoutes =  require('./routes/movieRoutes');
+app.use('/peliculas', movieRoutes);
+
+
+//Ruta user
+const createUserRoute = require('./routes/createUserRoute');
+app.use('/createuser', createUserRoute);
+
+
+module.exports=app;
--- api/index.js
+++ api/index.js
@@ -1,36 +1,12 @@
-const express = require('express');
-const cors = require('cors');
+const app = require('./app');
 const dotenv = require('dotenv').config();
 const { Sequelize }= require('sequelize');
 const sequelize = require('./config/database');
 
 
 
-const app = express();
-app.use(express.json()); //req.body usefull
-
-app.use(cors());
-
 const PORT = process.env.PORT || 5000;
 
-const router = express.Router();
-
-router.get('/', (req,res)=>{
-    res.json({
-        message: 'API estructuras de datos - cinema'
-    })
-});
-
-app.use('/', router);
-
-//Ruta movies
-const movieRoutes =  require('./routes/movieRoutes');
-app.use('/peliculas', movieRoutes);
-
-
-//Ruta user
-const createUserRoute = require('./routes/createUserRoute');
-app.use('/createuser', createUserRoute);
 
 
 //Database authenticate
