1eb0f33e49834a4a1deccca1e8c479a7dc4ba84e
andressalazar08 <andres.f.salazar@correounivalle.edu.co>
2024-09-24 18:28:31 -0500

    vite url

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -16,8 +16,8 @@ const Login = () => {
     
     // const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080';
         
-    const BASE_URL =  'http://localhost:8080';
-    //const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080';
+    //const BASE_URL =  'http://localhost:8080';
+    const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080';
     //console.log(BASE_URL);
     try {
       const response = await axios.post(`${BASE_URL}/user/loginUser`, { email, password });
