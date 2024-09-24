aec99b14f3dd793ad001d62628025eea3bc3ead7
andressalazar08 <andres.f.salazar@correounivalle.edu.co>
2024-09-24 16:01:53 -0500

    image on movies

--- frontend/src/components/movies/Movies.jsx
+++ frontend/src/components/movies/Movies.jsx
@@ -23,7 +23,16 @@ const Movies = () => {
     <div className="movie-list">
       {movies.map((movie) => (
         <div className="movie-card" key={movie.id}>
+          {/* <img src={movie.image_url} alt={movie.titulo} className="movie-image" /> */}
+          <img 
+      //src='https://imgc.allpostersimages.com/img/posters/dc-comics-the-dark-knight-batman-logo-on-fire-one-sheet-premium-poster_u-L-F9TM6L0.jpg'
+      src={movie.image_url}
+      alt={movie.titulo} 
+      className="movie-image" 
+      onError={(e) => { e.target.src = 'https://via.placeholder.com/300'; }} // Por si hay un error en la URL
+    />
           <h2>{movie.titulo}</h2>
+          <h6>{movie.image_url}</h6>
           <p>Año: {movie.anio}</p>
           <p>Duración: {movie.duracion} minutos</p>
           <p>Rating: {movie.rating}/5</p>
