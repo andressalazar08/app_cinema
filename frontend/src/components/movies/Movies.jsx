ce0af122ef47cff725e1b30c3de053e411d9ed20
andressalazar08 <andres.f.salazar@correounivalle.edu.co>
2024-09-26 11:17:27 -0500

    frontend component movie salas and horarios

--- frontend/src/components/movies/Movies.jsx
+++ frontend/src/components/movies/Movies.jsx
@@ -15,36 +15,49 @@ const Movies = () => {
     fetchMovies();
   }, []);
 
-  return (
+  rreturn (
     <>
-    <Navbar></Navbar> 
-    <div className="movies-container">
-      
-    <h1>Listado de Películas</h1>
-    <div className="movie-list">
-      {movies.map((movie) => (
-        <div className="movie-card" key={movie.id}>
-          {/* <img src={movie.image_url} alt={movie.titulo} className="movie-image" /> */}
-          <img 
-      //src='https://imgc.allpostersimages.com/img/posters/dc-comics-the-dark-knight-batman-logo-on-fire-one-sheet-premium-poster_u-L-F9TM6L0.jpg'
-      src={movie.image_url}
-      alt={movie.titulo} 
-      className="movie-image" 
-      onError={(e) => { e.target.src = 'https://via.placeholder.com/300'; }} // Por si hay un error en la URL
-    />
-          <h2>{movie.titulo}</h2>
-     
-          <p>Año: {movie.anio}</p>
-          <p>Duración: {movie.duracion} minutos</p>
-          <p>Rating: {movie.rating}/5</p>
-          <Link to={`/movies/${movie.id}`}>
-              <button>Ver detalles</button>
-            </Link>
+      <Navbar />
+      <div className="movies-container">
+        <h1>Listado de Películas</h1>
+        <div className="movie-list">
+          {movies.map((movie) => (
+            <div className="movie-card" key={movie.id}>
+              {/* Imagen de la película */}
+              <img 
+                src={movie.image_url}
+                alt={movie.titulo} 
+                className="movie-image" 
+                onError={(e) => { e.target.src = 'https://via.placeholder.com/300'; }} // Por si hay un error en la URL
+              />
+              {/* Información de la película */}
+              <h2>{movie.titulo}</h2>
+              <p>Año: {movie.anio}</p>
+              <p>Duración: {movie.duracion} minutos</p>
+              <p>Rating: {movie.rating}/5</p>
+
+              {/* Información de las salas y horarios */}
+              {movie.Salas && movie.Salas.length > 0 && (
+                <div className="salas-info">
+                  <h3>Salas y Horarios</h3>
+                  {movie.Salas.map((sala) => (
+                    <div key={sala.id} className="sala-card">
+                      <p><strong>Sala número:</strong> {sala.numero}</p>
+                      <p><strong>Horario:</strong> {new Date(sala.horario).toLocaleString()}</p>
+                    </div>
+                  ))}
+                </div>
+              )}
+
+              {/* Botón para ver detalles */}
+              <Link to={`/movies/${movie.id}`}>
+                <button>Ver detalles</button>
+              </Link>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
-  </div>
-  </>
+      </div>
+    </>
   );
 };
 
